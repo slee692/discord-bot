a76d4a66333263e9d19b3a0d622483060b8e35dd
slee692 <slee692@illinois.edu>
2022-01-23 13:30:34 -0600

    added idea function and calc function

---
+++ bot.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+import random
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name="idea", help="Get a random side project idea")
+async def idea(ctx):
+    await ctx.send("Ideas are hard")
+    await ctx.send("Worry not, you're gonna be fine")
+
+    topics = ["chat bot", "cli", "game", "web bot", "browser extension", "api", "website"]
+    areas = ["pet care", "doing homework", "fitness"]
+
+    idea = f'Create a new {random.choice(topics)} that helps with {random.choice(areas)} :slight_smile:'
+    await ctx.send(idea)
+
+@bot.command(name="calc", help="Do a two number operation where function is either +, -, *, or /.")
+async def calc(ctx, x: float, fn: str, y: float):
+    if fn == '+':
+        await ctx.send(x + y)
+    elif fn == '-':
+        await ctx.send(x - y)
+    elif fn == '*':
+        await ctx.send(x * y)
+    elif fn == '/':
+        await ctx.send(x / y)
+
+
+with open("BOT_TOKEN.txt", "r") as token_file:
+    TOKEN = token_file.read()
+    print("Token file read")
+    bot.run(TOKEN)
